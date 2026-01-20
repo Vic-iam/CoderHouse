e937aa3d7509f98d249091a0310e8a1a76a51bdb
Jose Vitriago <80923963+Vic-iam@users.noreply.github.com>
2026-01-20 12:03:18 -0300

    Update App.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import Home from "./routes/Home";
 import Contacto from "./routes/Contacto";
 import Footer from "./components/Footer";
-import ItemListContainer from "./components/itemListContainer";
+import ItemListContainer from "./components/ItemListContainer";
 import CartWidget from "./components/CartWidget";
 
 function App() {
