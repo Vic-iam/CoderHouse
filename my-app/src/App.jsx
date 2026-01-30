13d0ef16e14fb2caa9627f4504cfaba6d25f7af7
Vic-iam <80923963+Vic-iam@users.noreply.github.com>
2026-01-29 21:02:27 -0300

    j

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -15,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/ItemList" element={<ItemListContainer name="Sara" />} />
+        
         <Route path="/Contacto" element={<Contacto />} />
         <Route path="/Cart" element={<CartWidget />} />
       </Routes>
