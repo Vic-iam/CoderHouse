abdf63a998043d948b21b1e01e893d941511f20a
Vic-iam <80923963+Vic-iam@users.noreply.github.com>
2026-02-15 11:32:15 -0300

    Update Navbar.jsx

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -6,7 +6,6 @@ import CartWidget from "./CartWidget";
 import { FaChevronDown } from "react-icons/fa6";
 
 function Navbar() {
-
   const [isOpen, setIsOpen] = useState(false);
   const [showCat, setShowCat] = useState(false);
 
@@ -22,53 +21,43 @@ function Navbar() {
         <div className={style.divLogo}>
           <h1>tuManga</h1>
         </div>
-
-        <div
-          className={`${style.divLinks} ${isOpen ? style.open : ""}`}
-        >
-
-          <Link to="/" onClick={handleLinkClick}>Inicio</Link>
-
+        <div className={`${style.divLinks} ${isOpen ? style.open : ""}`}>
+          <Link to="/" onClick={handleLinkClick}>
+            Inicio
+          </Link>
           <div
             className={style.productosDropdown}
-            onClick={() => setShowCat(!showCat)}
-          >
-            <h2 style={{display: "flex", gap:"5px"}}>Productos <FaChevronDown /></h2>
-
+            onClick={() => setShowCat(!showCat)}>
+            <h2 style={{ display: "flex", gap: "5px" }}>
+              Productos <FaChevronDown />
+            </h2>
             {showCat && (
               <div className={style.dropdownMenu}>
                 <Link to="/Productos" onClick={handleLinkClick}>
                   Ver todos
                 </Link>
-
                 <Link to="/categoria/mas-populares" onClick={handleLinkClick}>
                   Más populares
                 </Link>
-
                 <Link to="/categoria/mas-vendido" onClick={handleLinkClick}>
                   Más vendidos
                 </Link>
-
                 <Link to="/categoria/nuevos" onClick={handleLinkClick}>
                   Nuevos
                 </Link>
               </div>
             )}
           </div>
-
           <CartWidget />
-
         </div>
 
         <div
           className={`${style.itemToggle} ${isOpen ? style.open : ""}`}
-          onClick={() => setIsOpen(!isOpen)}
-        >
+          onClick={() => setIsOpen(!isOpen)}>
           <span></span>
           <span></span>
           <span></span>
         </div>
-
       </nav>
     </header>
   );
